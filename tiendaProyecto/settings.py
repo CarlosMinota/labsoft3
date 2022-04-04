0a8049a1555d0bfd49c6e15de706bbbdf3aab434
Carlos Minota <milocaart@hotmail.com>
2022-04-04 17:51:32 -0500

    3er commit modelos y funciones

--- tiendaProyecto/settings.py
+++ tiendaProyecto/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'controlProyecto.apps.ControlproyectoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
