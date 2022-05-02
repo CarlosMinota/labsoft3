2144c337cc6f2075a1cd53b46f6a2583e3aa22be
Carlos Minota <milocaart@hotmail.com>
2022-05-02 17:39:03 -0500

    Se crean algunas vistas html (muy basicas)

--- controlProyecto/admin.py
+++ controlProyecto/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question, Choise, Compra, Producto, Cliente, Envio, Carrito, Ciudad, Descuento, Departamento
+
+admin.site.register(Question)
+admin.site.register(Choise)
+admin.site.register(Envio)
--- controlProyecto/views.py
+++ controlProyecto/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
+from .models import Question
+from django.template import loader
+from django.http import Http404
 
 
 def index(request):
-    return HttpResponse("Hola mundo! Estás en el index de control del proyecto!")
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('controlProyecto/index.html')
+    contex = {'lastest_question_list': lastest_question_list}
+    #output = ''.join([q.question_text for q in lastest_question_list])
+    #return HttpResponse(output)
+    return render(request, 'controlProyecto/index.html', contex)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("La pregunta NO existe!")
+    return render(request, 'controlProyecto/detail', {'question':question})
+
+def results(request, question_id):
+    response = "Esta es la pagina de resultados de la pregunta %s" %question_id
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse("Estas votando en la pregunta %s" %question_id)
